81239a14900cd50d3abdc8fa8c59fa92cf1639b7
WittBulter <nanazuimeng123@gmail.com>
2018-10-14 01:44:44 +0800

    feat: improve texture

--- constants.lua
+++ constants.lua
@@ -4,7 +4,6 @@ local Constants = addon:NewModule('Constants')
 local infos = {
     ADDON_BASE_NAME = 'Nioro',
     VERSION = '0.7.0',
-    HEALTH_BAR_TEXTURE = 'Interface\\AddOns\\Nioro\\media\\Raid',
     ICON_DEFAULT_WIDTH = 17.7,
     NATIVE_UNIT_FRAME_HEIGHT = 36,
     NATIVE_UNIT_FRAME_WIDTH = 72,
